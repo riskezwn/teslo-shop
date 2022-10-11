ac7bc0b6f840dcb625dc9476122ca4ea010a38ea
riskezwn <enriquegarciagasc@gmail.com>
2022-10-11 09:04:07 +0200

    Pay order and save in database

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosError } from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { IPaypal } from '../../../interfaces';
+import { Order } from '../../../models';
 
 type Data = {
   message: string
@@ -32,12 +35,47 @@ const getPaypalBearerToken = async (): Promise<string|null> => {
 };
 
 const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  // TODO: validar sesión usuario
+  // TODO: validar mongoId
+
   const paypalBearerToken = await getPaypalBearerToken();
 
   if (!paypalBearerToken) {
     return res.status(200).json({ message: 'Paypal unauthorized' });
   }
-  return res.status(200).json({ message: paypalBearerToken });
+
+  const { transactionId = '', orderId = '' } = req.body;
+
+  const { data } = await axios.get<IPaypal.PaypalCheckoutResponse>(`${process.env.PAYPAL_ORDERS_URL}/${transactionId}`, {
+    headers: {
+      Authorization: `Bearer ${paypalBearerToken}`,
+    },
+  });
+
+  if (data.status !== 'COMPLETED') {
+    return res.status(401).json({ message: 'Order unrecognized' });
+  }
+
+  await db.connect();
+  const order = await Order.findById(orderId);
+
+  if (data.status !== 'COMPLETED') {
+    await db.disconnect();
+    return res.status(400).json({ message: 'Order unrecognized' });
+  }
+
+  if (order?.orderSummary.total !== Number(data.purchase_units[0].amount.value)) {
+    await db.disconnect();
+    return res.status(400).json({ message: 'Error in the amounts when making the payment' });
+  }
+
+  order.transactionId = transactionId;
+  order.isPaid = true;
+  await order.save();
+
+  await db.disconnect();
+
+  return res.status(200).json({ message: 'Order paid' });
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
--- pages/order/[id].tsx
+++ pages/order/[id].tsx
@@ -7,23 +7,54 @@ import { CreditCardOffOutlined, CreditCardOutlined } from '@mui/icons-material';
 // eslint-disable-next-line camelcase
 import { unstable_getServerSession } from 'next-auth';
 import { PayPalButtons } from '@paypal/react-paypal-js';
+import { useRouter } from 'next/router';
 import { ShopLayout } from '../../components/layouts';
 import { CartList, OrderSummary } from '../../components/cart';
 import { authOptions } from '../api/auth/[...nextauth]';
 import { dbOrders } from '../../database';
 import { IOrder } from '../../interfaces';
 import countries from '../../utils/countries';
+import { tesloApi } from '../../api_base';
+
+export type OrderResponseBody = {
+  id: string;
+  status:
+      | 'COMPLETED'
+      | 'SAVED'
+      | 'APPROVED'
+      | 'VOIDED'
+      | 'COMPLETED'
+      | 'PAYER_ACTION_REQUIRED';
+};
 
 interface Props {
   order: IOrder
 }
 
 const OrderPage: NextPage<Props> = ({ order }) => {
+  const router = useRouter();
   const {
     _id, isPaid, orderSummary, shippingAddress, orderItems,
   } = order;
 
   const countryName = countries.find((c) => c.code === shippingAddress.country);
+
+  const onOrderCompleted = async (details: OrderResponseBody) => {
+    if (details.status !== 'COMPLETED') {
+      throw new Error('Order not paid in Paypal');
+    }
+
+    try {
+      const { data } = await tesloApi.post('/orders/pay', {
+        transactionId: details.id,
+        orderId: _id,
+      });
+
+      router.reload();
+    } catch (error) {
+      throw new Error(error as string);
+    }
+  };
 
   return (
     <ShopLayout title={`Order - ${_id}`} pageDescription="Your order summary">
@@ -103,10 +134,7 @@ const OrderPage: NextPage<Props> = ({ order }) => {
                         ],
                       })}
                       onApprove={(data, actions) => actions.order.capture().then((details) => {
-                        console.log({ details });
-
-                        const name = details.payer.name.given_name;
-                        alert(`Transaction completed by ${name}`);
+                        onOrderCompleted(details);
                       })}
                     />
                   )
